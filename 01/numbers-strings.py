24e2a729ec82dc60ba67347ca5e14faae07972b2
Shubhrendu Tripathi <11039534+shubhrendutripathi@users.noreply.github.com>
2020-04-15 00:53:33 +0530

    second commit

---
+++ 01/numbers-strings.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+#this is the first comment
+spam = 1 # and this is a second comment
+		 # ... and now a third!
+text = "# This is not a comment because it's inside quotes."
+
+#numbers
+
