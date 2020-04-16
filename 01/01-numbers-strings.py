ce442d0522e090442ad8b2543787047e4125974a
Shubhrendu Tripathi <11039534+shubhrendutripathi@users.noreply.github.com>
2020-04-16 14:04:15 +0530

    updated

---
+++ 01/01-numbers-strings.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+#this is the first comment
+spam = 1 # and this is a second comment
+		 # ... and now a third!
+text = "# This is not a comment because it's inside quotes."
+
+#numbers
+print(2 + 2)
+
+print(50 - 5*6)
+
+print((50 - 5*6) / 4)
+
+print(8 / 5) # division always returns a floating point number
+
+print(17 / 3) # classic division returns a float
+
+print(17 // 3) # floor division discards the fractional part
+
+print(17 % 3) # the % operator returns the remainder of the division
+
+print(5 * 3 + 2) # result * divisor + remainder
+
+print(5 ** 2) # 5 squared
+
+print(2 ** 7) # 2 to the power of 7
+
+width = 20
+height = 5 * 9
+print(width * height)
+
+
