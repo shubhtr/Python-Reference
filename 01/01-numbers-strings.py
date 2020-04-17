5cc16f61aa8d820053d70dfc3f92711329473f27
Shubhrendu Tripathi <11039534+shubhrendutripathi@users.noreply.github.com>
2020-04-17 19:21:18 +0530

    another commit

--- 01/01-numbers-strings.py
+++ 01/01-numbers-strings.py
@@ -5,7 +5,7 @@ spam = 1 # and this is a second comment
 		 # ... and now a third!
 text = "# This is not a comment because it's inside quotes."
 
-#numbers
+#################################### numbers
 print(2 + 2)
 
 print(50 - 5*6)
@@ -29,5 +29,50 @@ print(2 ** 7) # 2 to the power of 7
 width = 20
 height = 5 * 9
 print(width * height)
+
+
+#operators with mixed type operands convert the integer operand to floating point:
+print(4 * 3.75 - 1)
+
+#three distinct numeric types - integers, floating point numbers and complex numbers
+#int, float, Decimal, Fraction, etc
+#complex numbers - uses the j or J suffix for the imaginary part. eg 3+5j
+
+
+######################################### strings
+
+print('spam eggs') # single quotes
+
+print('doesn\'t') # use \' to escape the single quote
+
+print("doesn't") # ... or use double quotes instead
+
+print('"Yes," they said.')
+
+print("\"Yes,\" they said.")
+
+print('"Isn\'t," they said.')
+
+s = 'First line.\nSecond line.' # \n means newline
+print (s)
+
+print('C:\some\name') # here \n means newline!
+
+print(r'C:\some\name') # note the r before the quote - for 'raw strings'
+
+# string literals spanning multiple lines - triple quotes: """...""" or '''...'''
+print("""\
+Usage: thingy [OPTIONS]
+	-h						Display this usage message
+	-H	hostname			Hostname to connect to 
+""")
+
+# strings can be concatenated together with the + operator, and repeated with *
+
+# 3 times 'un', followed by 'ium'
+print(3 * 'un' + 'ium')
+
+
+
 
 
