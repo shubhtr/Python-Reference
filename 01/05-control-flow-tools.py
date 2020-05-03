d9d01ceae8e487a305a71036cb9f42553ef62de1
Shubhrendu Tripathi <11039534+shubhrendutripathi@users.noreply.github.com>
2020-05-03 09:55:11 +0530

    updated control flow tools

--- 01/05-control-flow-tools.py
+++ 01/05-control-flow-tools.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+
+######################################################### if
+
 x = int(input("Please enter an integer: "))
 
 if x < 0:
@@ -13,4 +16,50 @@ else:
 	print('More')
 
 # if...elif...elif ... sequence is a substitute for the switch or case statements found in other languages
+
+######################################################### for
+
+# measure some strings
+words = ['cat', 'window', 'defenestrate']
+
+for w in words:
+	print(w, len(w))
+
+# modify a collection while iterating over that same collection
+
+users = {"dog" : "active", 
+			"cat" : "inactive", 
+			"horse" : "active", 
+			"pigeon" : "active"}
+
+print (users)
+
+# strategy: iterate over a copy
+for user, status in users.copy().items():
+	if status == 'inactive':
+		del users[user]
+
+print (users)
+
+# strategy: create a new collection
+active_users = {}
+for user, status in users.items():
+	if status == 'active':
+		active_users[user] = status
+
+
+######################################################## range
+
+for i in range(5):
+	print (i)
+
+
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+for i in range(len(a)):
+	print (i, a[i])
+
+print (range(4))
+print (list(range(4)))
+
+
 
