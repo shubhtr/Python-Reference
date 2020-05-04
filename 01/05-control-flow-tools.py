0df9872067d383475e059e5f83f270748ecfc3b9
Shubhrendu Tripathi <11039534+shubhrendutripathi@users.noreply.github.com>
2020-05-05 01:58:59 +0530

    updated control flow tools

--- 01/05-control-flow-tools.py
+++ 01/05-control-flow-tools.py
@@ -59,7 +59,49 @@ for i in range(len(a)):
 	print (i, a[i])
 
 print (range(4))
+
+print (sum(range(4)))
+
 print (list(range(4)))
+
+# loop searches for prime numbers
+for n in range(2,10):
+	for x in range(2,n):
+		if n % x == 0:
+			print(n, 'equals', x, '*', n//x)
+			break
+	else:
+		# loop fell through without finding a factor
+		print(n, 'is a prime number')
+
+# this is correct code - the else clause belongs to the for loop, not the if statement
+# a loop's else clause runs when no break occurs (more like the else clause of a try statement)
+
+
+# continue statement - continues with the next iteration of the loop 
+# - so it skips the rest of the statements that come after it 
+for num in range(2, 10):
+	if num % 2 == 0:
+		print("Found an even number", num)
+		continue
+	print("Found a number", num)
+
+
+# pass statement does nothing. it can be used when a statement is required syntactically
+# but the program requires no action
+
+# while True:
+#	pass			# Busy-wait for keyboard interrupt (Ctrl+C)
+
+# commonly used for creating minimal classes
+class MyEmptyClass:
+	pass
+
+# used as a place-holder for a function or conditional body when working on new code, 
+# allowing you to keep thinking at a more abstract level. the pass is silently ignored.
+def initlog(*args):
+	pass			# remember to implement this!
+
 
 
 
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Python-Reference
 This repository contains a collection of sample code, recipes, snippets, etc for the Python 3 programming language.
 
+[![made-with-python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg)](https://www.python.org/)
+
 # Author
 Shubhrendu Tripathi
 
