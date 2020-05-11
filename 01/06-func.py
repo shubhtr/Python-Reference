4526c5f01a287acf77bf552053394d519debff1d
Shubhrendu Tripathi <11039534+shubhrendutripathi@users.noreply.github.com>
2020-05-11 13:31:27 +0530

    updated func

--- 01/06-func.py
+++ 01/06-func.py
@@ -35,3 +35,53 @@ f100 = fib2(100)	# call it
 print (f100)		# write the result
 
 
+# default argument values
+def ask_ok(prompt, retries=4, reminder='Please try again!'):
+	while True:
+		ok = input(prompt)
+		if ok in ('y', 'ye', 'yes'):
+			return True
+		if ok in ('n', 'no', 'nop', 'nope'):
+			return False
+		retries = retries - 1
+		if retries < 0:
+			raise ValueError('invalid user response')
+		print (reminder)
+
+ask_ok('Do you really want to quit?')
+ask_ok('OK to overwrite the file?', 2)
+ask_ok('OK to overwrite the file?', 2, 'Come on, only yes or no!')
+
+# in keyword - test whether or not a sequence contains a certain value
+
+# scope
+i = 5
+
+def f(arg=i):
+	print (arg)
+
+i = 6
+f()
+
+# IMPORTANT WARNING - the default value is evaluated only once. example below is L
+
+def f(a, L=[]):
+	L.append(a)
+	return L
+
+print (f(1))
+print (f(2))
+print (f(3))
+
+# if don't want to share between subsequent calls
+def f(a, L=None):
+	if L is None:
+		L = []
+	L.append(a)
+	return L
+
+print (f(1))
+print (f(2))
+print (f(3))
+
+
